e115a5298fd3b302d2e400e42b965c783d7eeb7b
Subhasis-Padhy-2004 <subhsispadhy@gmail.com>
2024-04-02 22:55:37 +0530

    first commit

---
+++ dashboard.php
@@ -0,0 +1,38 @@
+<?php
+require ('inc/essentials.php');
+adminLogin();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel - Dashboard</title>
+    <?php require ('inc/links.php'); ?>
+</head>
+
+<body class="bg-light">
+
+    <?php
+    require ('inc/header.php'); ?>
+
+    <div class="container-fluid" id="main-content">
+        <div class="row">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit fugiat ullam, quia facilis
+                illum nesciunt corporis impedit blanditiis reiciendis. Temporibus et quisquam similique recusandae,
+                doloribus nemo culpa officiis exercitationem explicabo, eos porro delectus pariatur impedit. A ipsa
+                doloribus aperiam provident, voluptas quisquam, deserunt ad veritatis praesentium, nihil commodi
+                debitis.
+            </div>
+        </div>
+    </div>
+
+
+    <?php require ('inc/scripts.php'); ?>
+</body>
+
+</html>
